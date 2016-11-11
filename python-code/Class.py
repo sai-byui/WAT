d55b3e869891f7da04df81b8c61573156b3d7bf5
Landon Shumway <altruist8@yahoo.com>
2016-11-10 23:38:14 -0700

    Created test file using other classes
    This file will hold all of the test python code we create using our word
    classes.

--- python-code/Class.py
+++ python-code/Class.py
@@ -2,34 +2,37 @@ import os
 
 
 
+
 class Class:
     def __init__(self, class_name, file_name = None):
         self.class_name = class_name
+        # if no file name is passed, we are making a new file titled after the class
         if file_name == None:
-            self.file_name = class_name
+            self.file_name = "%s.py" % class_name
+            self.target = open(self.file_name, 'w+')
+            self.is_new_file = True
         else:
-            self.file_name =file_name
-
-        self.target = open(self.file_name, 'w+')
-
-
+            self.file_name = file_name
+            self.target = open(self.file_name, 'a+')
+            self.is_new_file = False
 
 
-    def create(self,class_name, function_name):
-        #top of file
-        self.target.write('import os\n\n\n')
+
+    def create(self):
+        # give some spacing from any previous code
+        if self.is_new_file == True:
+            self.target.write('import os\n\n\n')
+        else:
+            # make some spacing
+            self.target.write('\n\n\n')
         #write the title of the class
-        self.target.write('class %s:\n' % class_name)
+        self.target.write('class %s:\n' % self.class_name)
         #default constructor
         self.target.write('\tdef __init__(self):\n')
         self.target.write('\t\tpass\n\n')
-        self.target.write('\t def %s():' % function_name)
 
 
 
+test = Class("test")
 
-
-
-test = Class("test","test.py")
-
-test.create("test", "hello_world")
+test.create()
